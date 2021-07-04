738bc6cccb47c9a7fc78831dd7b89bca8e464500
Mr Strange <77616081+Strangx@users.noreply.github.com>
2021-07-05 00:30:48 +0630

    Create clsp.site.js

---
+++ Script/clsp.site.js
@@ -0,0 +1,2 @@
+#https://sppsge.xyz?p=OG8LDJ
+^https:\/\/app\.(.+)\.me\/api\/.+ url request-header (\r\n)Authorization:.+(\r\n) request-header $1Authorization:eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjQ1MzA5NiIsImlzcyI6IiIsImlhdCI6MTYxODU4NTc2OSwibmJmIjoxNjE4NTg1NzY5LCJleHAiOjE3NzYyNjU3Njl9.NvngndWmUawhDMllxDQKEmG1WUwyrkPnasztVOL088o$2
