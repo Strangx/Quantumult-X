350036c5704aec5df6eda2c18d6c710c25a6d800
Mr Strange <77616081+Strangx@users.noreply.github.com>
2021-07-13 22:49:18 +0630

    Update resource-parser.js

--- resource-parser.js
+++ resource-parser.js
@@ -14,7 +14,7 @@
 // $done({error : "error description"});
 // $done({content : "the modified content"});
 
-var sampleA = "shadowsocks= sg-sng-st003.prod.surfshark.com:44467, method=aes-256-gcm, password=hWPYUXPGNxLxyqW6jC2FnueU, obfs=http, obfs-host=bing.com, obfs-uri=/resource/file, fast-open=false, udp-relay=false, tag=Tag-A";
-var sampleB = "shadowsocks= 92.119.177.23:44467, method=aes-256-gcm, password=hWPYUXPGNxLxyqW6jC2FnueU, obfs=http, obfs-host=bing.com, obfs-uri=/resource/file, fast-open=false, udp-relay=false, tag=Tag-B";
+var sampleA = "shadowsocks= sg-sng-st003.prod.surfshark.com:44467, method=aes-256-gcm, password=hWPYUXPGNxLxyqW6jC2FnueU, fast-open=false, udp-relay=false, tag=Tag-A";
+var sampleB = "shadowsocks= 92.119.177.23:44467, method=aes-256-gcm, password=hWPYUXPGNxLxyqW6jC2FnueU, fast-open=false, udp-relay=false, tag=Tag-B";
 var total = sampleA + "\n" + sampleB;
 $done({content : total});
